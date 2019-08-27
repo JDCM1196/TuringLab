b05df2a5c42aab9d1452e9cb12a8d93d0aa4e982
Juan Diego Cardona Mena <15000787@galileo.edu>
2019-08-27 16:15:05 -0600

    Added operations for xh, xb and y

--- GalileoHand_Observer/main.c
+++ GalileoHand_Observer/main.c
@@ -36,35 +36,65 @@
 
 fingers middle_f =  {WAITC, 3, 0, 0, 0, 62};
 
+#define		SysTickFreq		100
+#define		PITFreq				 12
 uint32_t ticks = 0;                                                         // 1 ms ticks
 															
 QuaD QD;
 volatile float32_t w = 0;
 float32_t V = 0;
-float32_t I = 0;	
+float32_t In = 0;
+float32_t Ib = 0;
 float32_t t = 0;
 float32_t theta = 0;
+float32_t omegaG = 0;
 
-char command[30]; 
+char command[50]; 
 
 //Inicialización de las matrices de estado
 const uint8_t R = 35;
 const float32_t L = 1.35f*10E-3;
 const float32_t k = 3.658f*10E-3;
-const float32_t J = 3.823f*10E-12;
+//const float32_t J = 3.823f*10E-12;
 const float32_t G = 248.98f;
 const float32_t r = 4.1f*10E-3;
 const float32_t rp = 4.5f*10E-3;
 const float32_t Rs = 3.66f;
 
 //Estados (en orden): theta, omega, corriente
-float32_t A[3][3] = {0,	1,	0,
-										 0,	0,	k/J,
-										 0,	-k/L,	-R/L};
-float32_t B[3][1] = {0,
-										 0,
-										 1/L};
-float32_t C[1][3] = {0,	0,	1};
+arm_matrix_instance_f32 x;
+float32_t x_f32[3][1] = {0,
+													0,
+													0};
+
+arm_matrix_instance_f32 xb;
+float32_t xb_f32[3][1] = {0,
+													0,
+													0};
+
+arm_matrix_instance_f32 xh;
+float32_t xh_f32[3][1] = {0,
+													0,
+													0};
+
+arm_matrix_instance_f32 y;
+float32_t y_f32[1][1] = {0};
+
+arm_matrix_instance_f32 I;
+
+//Matrices del sistema (ya discretizadas)
+arm_matrix_instance_f32 A;
+float32_t A_f32[3][3] = {1,	9.9993E-6,	0.3856,
+												0,	0,	0,
+												0,	0,	0};
+
+arm_matrix_instance_f32 B;
+float32_t B_f32[3][1] = {2.8332,
+												283.6075,
+												-4.3263E-19};
+
+arm_matrix_instance_f32 C;
+float32_t C_f32[1][3] = {0,	0,	1};
 
 //Las últimas dos salidas son la fuerza y el torque ejercidos por los joints
 float32_t Co[5][3] = {1/G,	0,	0,
@@ -75,24 +105,24 @@ float32_t Co[5][3] = {1/G,	0,	0,
 
 float32_t Cc[1][3] = {0,	0,	-k*G/rp};
 
-float32_t LG[3][1] = {-0.0177,
-											0.0016,
-											-0.9082};
+arm_matrix_instance_f32 LG;
+float32_t LG_f32[3][1] = {-0.0296,
+											-2.1085,
+											-0.8462};
 
 int main(void){
   LED_Config(); 
   ADC0_Config();
   UART0_Config();
-	UART1_Config();
   Output_Config();
-  SysTick_Config(SystemCoreClock/100);
-  PIT_Init(24);
+	LED_On();
+  SysTick_Config(SystemCoreClock/SysTickFreq);
+  PIT_Init(PITFreq);
 
   arm_fill_q15(0, middle_f.buffer, SIZE);
 
 	QD_Init(&QD);
 	Finger_Open(middle_f.finger_m);
-	LED_On();
 	
 	while(1){
 		/*if(w != QD.omega){
@@ -103,16 +133,41 @@ int main(void){
 }
 void SysTick_Handler(void) {
 	V = ADC0_Read(4)*3.3f/1024.0f;
-	I = V/Rs;
-	theta = FTM1->CNT*PI/(G*6);
+	In = V/Rs;
+	float32_t I_f32[1][1] = {In};
+	theta = FTM1->CNT*PI/(G*6.0f);
+	omegaG = QD.omega/G;
 	
-	sprintf(command, "%.2f,%.4f,%.2f,%.2f\r", V, I, QD.omega, theta);
+	x_f32[0][0] = theta;
+	x_f32[1][0] = omegaG;
+	x_f32[2][0] = In;
+	
+	sprintf(command, "%.2f,%.4f,%.2f,%.2f\r", V, In, omegaG, theta);
 	UART0_putString(command);
+
+	arm_mat_init_f32(&x, 3, 1, (float32_t *)x_f32);
+	arm_mat_init_f32(&xb, 3, 1, (float32_t *)xb_f32);
+	arm_mat_init_f32(&xh, 3, 1, (float32_t *)xh_f32);	
+	arm_mat_init_f32(&I, 1, 1, (float32_t *)I_f32);	
 	
+	//xb = Ax+B(11.77-I*R-L(I-Iant)/SysTickFreq)
+	arm_mat_mult_f32(&A, &x, &xb);
+	arm_mat_scale_f32(&B, 11.77 - In*R - L*(In-Ib)/SysTickFreq, &B);
+	arm_mat_add_f32(&xb, &B, &xb);
+	
+	//y = Cx
+	arm_mat_mult_f32(&C, &x, &y);
+	
+	//xh = xb-LG(y+I)
+	arm_mat_add_f32(&y, &I, &y);
+	arm_mat_mult_f32(&y, &LG, &xh);
+	arm_mat_scale_f32(&xh, -1, &xh);
+	arm_mat_add_f32(&xb, &xh, &xh);
+
+	Ib = In;
 	Finger_Timing(&middle_f);
 	ticks++; 
 }
-
 
 void PIT0_IRQHandler(){
 	//char string[50];
